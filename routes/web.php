3b969bca6161a7079f29131df48063e02b5f675c
HellenGuzman <hellenaliciaguzmanhuezo@gmail.com>
2026-07-08 17:58:27 -0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -77,4 +77,124 @@ Route::get('/medicamentos', function () {
     return $html;
 });
 
+Route::get('/clientes/vip', function () {
+    $clientes = [
+        (object)['id' => 1, 'nombre' => 'Karen Criollo', 'telefono' => '+503 789789', 'puntos_acumulados' => 15],
+        (object)['id' => 2, 'nombre' => 'Hellen Guzman', 'telefono' => '+503 666006', 'puntos_acumulados' => 5],
+        (object)['id' => 3, 'nombre' => 'Maria Lopez', 'telefono' => '+503 1122233', 'puntos_acumulados' => 3],
+    ];
+
+    $html = '
+    <table border="1" cellspacing="0">
+        <thead>
+            <tr>
+                <th>ID Cliente</th>
+                <th>Nombre</th>
+                <th>Telefono</th>
+                <th>Puntos Acumulados</th>
+            </tr>
+    ';
+
+    foreach ($clientes as $cliente) {
+        $html .= "
+            <tr>
+                 <td>{$cliente->id}</td>
+                 <td>{$cliente->nombre}</td>
+                 <td>{$cliente->telefono}</td>
+                 <td>{$cliente->puntos_acumulados}</td>
+            </tr>
+        ";
+    }
+
+    $html .= '
+        </tbody>
+    </table>
+    ';
+
+     //Pintamos enla ventana del navegador de la tabla
+    echo $html;
+});
+
+Route::get('/proveedores/internacionales', function () {
+
+    $proveedores = [
+        (object)['empresa' => 'Farmacias Similares', 'pais_origen' => 'México', 'medicamento_principal' => 'acetaminofén', 'tiempo_entrega_dias' => 15],
+        (object)['empresa' => 'Farmacias Camila', 'pais_origen' => 'El Salvador', 'medicamento_principal' => 'ibuprofeno', 'tiempo_entrega_dias' => 22],
+        (object)['empresa' => 'FarmaNic', 'pais_origen' => 'Nicaragua', 'medicamento_principal' => 'amoxicilina', 'tiempo_entrega_dias' => 7]
+    ];
+
+    $html = '
+    <table border="1" cellspacing="0">
+        <thead>
+            <tr>
+                <th>Empresa</th>
+                <th>País de origen</th>
+                <th>Medicamento principal</th>
+                <th>Tiempo de entrega (días)</th>
+            </tr>
+        </head>
+    ';
+
+    foreach ($proveedores as $proveedor) {
+        $advertencia = $proveedor->tiempo_entrega_dias > 15 ? ' <strong>(Demora Crítica)</strong>' : '';
+        $html .= "
+            <tr>
+                <td>{$proveedor->empresa}</td>
+                <td>{$proveedor->pais_origen}</td>
+                <td>{$proveedor->medicamento_principal}</td>
+                 <td>{$proveedor->tiempo_entrega_dias}{$advertencia}</td>
+            </tr>
+        ";
+    }
+
+    $html .= '
+        </tbody>
+    </table>
+    ';
+
+    echo $html;
+});
+
+Route::get('/lotes/inventario', function () {
+
+    $lotes = [
+        (object)['codigo_lote' => 'L01', 'nombre_medicamento' => 'Insulina Glargina', 'cantidad_cajas' => 110, 'temperatura_requerida_celsius' => 4],
+        (object)['codigo_lote' => 'L02', 'nombre_medicamento' => 'ibuprofeno', 'cantidad_cajas' => 30, 'temperatura_requerida_celsius' => 20],
+        (object)['codigo_lote' => 'L03', 'nombre_medicamento' => 'Vacuna influenza', 'cantidad_cajas' => 112, 'temperatura_requerida_celsius' => 2]
+    ];
+
+    $html = '
+    <table border = "1" cellpading="0">
+        <thead>
+            <tr>
+                <th>Códig
+                o de Lote</th>
+                <th>Nombre del Medicamento</th>
+                <th>Cantidad de Cajas</th>
+                <th>Temperatura Requerida en celsius (°C)</th>
+            </tr>
+        </thead>
+        <tbody>
+    ';
+
+    foreach ($lotes as $lote) {
+        $etiqueta = $lote->temperatura_requerida_celsius <= 5 ? ' <strong>[Requiere Cadena de Frío]</strong>' : '';
+
+        $html .= "
+            <tr>
+                <td>{$lote->codigo_lote}</td>
+                <td>{$lote->nombre_medicamento}{$etiqueta}</td>
+                <td>{$lote->cantidad_cajas}</td>
+                <td>{$lote->temperatura_requerida_celsius}</td>
+            </tr>
+        ";
+    }
+
+    $html .= '
+        </tbody>
+    </table>
+    ';
+
+    echo $html;
+});
 require __DIR__.'/settings.php';
